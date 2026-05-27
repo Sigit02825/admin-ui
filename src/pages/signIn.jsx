aa93e683d73ca139360e9b1c2d2f58c20677ef20
Sigit02825 <sigitilham00@gmail.com>
2026-05-27 17:18:27 +0700

    atomic design page sign in dan sign up

---
+++ src/pages/signIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthLayout from "../components/Layouts/AuthLayout";
+import FormSignIn from "../components/Fragments/FormSignIn";
+function signIn() {
+  return (
+    <AuthLayout>
+        <FormSignIn />
+    </AuthLayout>
+  );
+}
+
+export default signIn;
